f3a0237dd6e0a681acad490a24a5958e56821aaf
Abdelmajid-Sbai <abdosbai2162@gmail.com>
2024-03-18 00:39:50 +0100

    message

--- src/Components/MiniNavbar.js
+++ src/Components/MiniNavbar.js
@@ -8,9 +8,9 @@ export default function MiniNavbar() {
                 <div className="firstsection  col-lg-6 col-sm-12">
                     <h6><span></span> Hotel Charf - 25 Rue al Farabi, 4 éme étage, Tangier, Morocco</h6>
                 </div>
-                <div className="secondsection row d-flex text-center   col-lg-6 col-sm-12">
-                    <h6 className="col-6"><span></span> +212 711247341</h6>
-                    <h6 className="col-6"><span></span> new.city.restaurant@gmail.com</h6>
+                <div className="secondsection row d-flex text-start   col-lg-6 col-sm-12">
+                    <h6 className="col-5"><span></span> +212 711247341</h6>
+                    <h6 className="col-7 text-center"><span></span> new.city.restaurant@gmail.com</h6>
                 </div>
             </div>
 
